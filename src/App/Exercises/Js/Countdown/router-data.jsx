2be8e01e7e727570630af4517a69690eed88e5e5
Katarzyna Winklas <winklas.k@gmail.com>
2023-10-15 21:40:56 +0200

    countdown added

---
+++ src/App/Exercises/Js/Countdown/router-data.jsx
@@ -0,0 +1,15 @@
+import { Countdown } from './index';
+
+export const countdownMetaData = {
+  path: 'js-countdown',
+
+  date: '12.10.2023',
+
+  linkLabel: 'JS - countdown',
+
+  blockNo: 21,
+
+  element: <Countdown />,
+
+  tags: ['setInterval', 'interval', 'time'],
+};
